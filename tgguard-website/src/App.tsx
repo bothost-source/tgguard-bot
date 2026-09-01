1bfdfb9f463a14c19791970a7f7947ef8cc25570
bothost-source <destinyjob493@gmail.com>
2026-09-01 19:06:28 +0100

    Update App.tsx

--- tgguard-website/src/App.tsx
+++ tgguard-website/src/App.tsx
@@ -1,33 +1,50 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import Dashboard from './pages/Dashboard'
 import OwnerDashboard from './pages/OwnerDashboard'
-import ProtectedRoute from './components/ProtectedRoute'
 import FAQ from './pages/FAQ'
 import Documentation from './pages/Documentation'
+import ProtectedRoute from './components/ProtectedRoute'
 
-export default function App() {
+function App() {
+  const location = useLocation()
+
   return (
     <AnimatePresence mode="wait">
-      <Routes>
+      <Routes location={location} key={location.pathname}>
+        {/* Public routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/docs" element={<Documentation />} />
-        <Route path="/dashboard/*" element={
-          <ProtectedRoute allowedRoles={['community_admin']}>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/owner/dashboard/*" element={
-          <ProtectedRoute allowedRoles={['owner']}>
-            <OwnerDashboard />
-          </ProtectedRoute>
-        } />
+
+        {/* Community Admin Dashboard */}
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute allowedRoles={['community_admin']}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Owner Dashboard */}
+        <Route
+          path="/owner/dashboard/*"
+          element={
+            <ProtectedRoute allowedRoles={['owner']}>
+              <OwnerDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Catch all - redirect to home */}
+        <Route path="*" element={<LandingPage />} />
       </Routes>
     </AnimatePresence>
   )
 }
 
+export default App
