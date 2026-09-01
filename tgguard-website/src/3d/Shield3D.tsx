8a4c180cea381b2b359818511e1d3c0219cb332d
bothost-source <destinyjob493@gmail.com>
2026-09-01 02:05:55 +0100

    Update Shield3D.tsx

--- tgguard-website/src/3d/Shield3D.tsx
+++ tgguard-website/src/3d/Shield3D.tsx
@@ -1,6 +1,6 @@
 import { useRef, useMemo } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { Float, MeshTransmissionMaterial, Text, RoundedBox } from '@react-three/drei'
+import { Float, MeshTransmissionMaterial, Text } from '@react-three/drei'
 import * as THREE from 'three'
 
 export function Shield3D({ position = [0, 0, 0] as [number, number, number] }) {
