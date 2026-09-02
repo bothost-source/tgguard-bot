6137dffeb9249cc3f3cb07f59aa83a7ccd270ea5
LONER <destinyjob493@gmail.com>
2026-09-02 23:06:24 +0100

    Update Shield3D.tsx

--- tgguard-website/src/3d/Shield3D.tsx
+++ tgguard-website/src/3d/Shield3D.tsx
@@ -1,6 +1,6 @@
 import { useRef, useMemo } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { Float, MeshTransmissionMaterial, Text } from '@react-three/fiber'
+import { Float, MeshTransmissionMaterial, Text } from '@react-three/drei'
 import * as THREE from 'three'
 
 interface CubeData {
